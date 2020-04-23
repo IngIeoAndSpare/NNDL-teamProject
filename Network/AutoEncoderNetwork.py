065ed1a3c9317eb86ea891c829ef57f12a0aef25
IngIeoAndSpare <wjh4877@naver.com>
2020-04-23 10:46:54 +0900

    inpainting Network init

---
+++ Network/AutoEncoderNetwork.py
@@ -0,0 +1,13 @@
+## torch module
+import torch.nn as nn
+
+class AutoEncoderNetwork:
+    '''
+        CAUTION : this class is Network. so, you coding only Network method and sub module.
+    '''
+
+    def __init__(self):
+        '''
+            TODO : params init
+            ex ->   self.{{PARAMS_NAME}} = {{PARAMS_INIT_VALUE}}
+        '''
