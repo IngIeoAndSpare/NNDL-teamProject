2471ff5668d872c2e1efc945b53fc90183fc46c5
IngIeoAndSpare <wjh4877@naver.com>
2020-05-04 18:07:31 +0900

    url ref image getter add

--- Utils/RefImageModule.py
+++ Utils/RefImageModule.py
@@ -1,9 +1,16 @@
 # image reader
-from PIL import Image
 import os
+import requests
+
+import cv2
+from PIL import Image
+import numpy as np
+
 
 class RefImageModule:
 
+    KEY = ""
+    
     def __init__(self, file_path, image_encoding, get_method = True):
         
         ## Path params
@@ -12,7 +19,9 @@ class RefImageModule:
         ## Image params
         self.image_encoding = image_encoding # RGB, RGBA
         self.image_file_format = "png"
-    
+
+        self.url_context = "http://xdworld.vworld.kr:8080/XDServer/3DData?Version=2.0.0.0&Request=GetLayer&Layer=tile&"
+        
         ## File getter 
         ## self.get_file_method_flag = get_method
 
@@ -54,10 +63,18 @@ class RefImageModule:
         image_path = f"{self.path_root_file_path}\{zoom_level}\{y_coordinate}\{y_coordinate}_{x_coordinate}.{self.image_file_format}"
         return Image.open(image_path).convert(self.image_encoding)
 
-'''
-    def _get_image_from_url(self, url):
-        ## TODO : get image use vworld url
-'''
+    def _get_image_from_url(self, x_coordinate, y_coordinate, zoom_level):
+        
+        string_query_context = f"level={zoom_level}&IDX={x_coordinate}&IDY={y_coordinate}&Key={KEY}"
+        url = ''.join([self.url_context, string_query_context])
+
+        im_content = requests.get(url).content
+        img_array = np.fromstring(im_content, np.uint8)
+
+        ##https://stackoverflow.com/questions/17170752/python-opencv-load-image-from-byte-string
+        img_np = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+        return img_np
 
 
 
